f7d4a9acd6f21a733ad55e34493370fbbe042667
gllanos09 <angel.llanos@tecsup.edu.pe>
2026-06-05 15:22:10 -0500

    Update

--- app/src/main/java/com/tecsup/authfirebaseappllanos/AuthApp.kt
+++ app/src/main/java/com/tecsup/authfirebaseappllanos/AuthApp.kt
@@ -6,14 +6,12 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.tecsup.authfirebaseappllanos.entities.CursosScreen
-import com.tecsup.authfirebaseappllanos.ui.screen.HomeScreen
 import com.tecsup.authfirebaseappllanos.ui.screen.LoginScreen
 import com.tecsup.authfirebaseappllanos.ui.screen.RegisterScreen
 
 object Destinations {
     const val LOGIN = "login"
     const val REGISTER = "register"
-    const val HOME = "home"
     const val CURSOS = "curso"
 }
 
@@ -51,16 +49,6 @@ fun AuthNavGraph(navController: NavHostController) {
                 },
                 onNavigateBack = {
                     navController.popBackStack()
-                }
-            )
-        }
-
-        composable(Destinations.HOME) {
-            HomeScreen(
-                onLogout = {
-                    navController.navigate(Destinations.LOGIN) {
-                        popUpTo(Destinations.HOME) { inclusive = true }
-                    }
                 }
             )
         }
